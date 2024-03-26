6aef1071a33dd1437832e3487ac67fbc181a8636
LuizFernandoFagundes <fernandopoa06@gmail.com>
2024-03-26 05:06:47 -0300

    ajuste da logica do arquivo .js

--- src/js/index.js
+++ src/js/index.js
@@ -19,7 +19,7 @@ const msnconfirmacao = document.querySelector('.mensagemconfirmacao');
 
 const erro = document.querySelectorAll('.erro');
 const inputs = document.querySelectorAll('.inputs');
-
+let estadobotao=true;
 
 
 campoNome.addEventListener('keyup', function(){
@@ -27,10 +27,13 @@ campoNome.addEventListener('keyup', function(){
         if(campoNome.value != "")
         {
             nomeCartao.innerHTML = campoNome.value;
+            erro[0].classList.add('hidden');
+            inputs[0].style.border='1px solid var(--Light-grayish-violet)';
         }
         else
         {
             nomeCartao.innerHTML="Luiz F S Fagundes";
+            erro[0].classList.remove('hidden');
         }
         
     
@@ -40,6 +43,8 @@ campoNumero.addEventListener('keyup', function(){
     
     if(campoNumero.value != ""){
     numeroCartao.innerHTML=campoNumero.value.substring(0,4)+" "+campoNumero.value.substring(4,8)+" "+campoNumero.value.substring(8,12)+" "+campoNumero.value.substring(12,16);
+    erro[1].classList.add('hidden');
+    inputs[1].style.border='1px solid var(--Light-grayish-violet)';
     }
     else{
         numeroCartao.innerHTML="0000 0000 0000 0000";
@@ -51,6 +56,9 @@ campoano.addEventListener('keyup', function(){
     if(campomes.value != "" && campoano.value != "")
     {
     dataCartao.innerHTML=`${campomes.value}/${campoano.value}`;
+    erro[2].classList.add('hidden');
+    inputs[2].style.border='1px solid var(--Light-grayish-violet)';
+    inputs[3].style.border='1px solid var(--Light-grayish-violet)';
     }
     else{
         dataCartao.innerHTML="00/00";
@@ -64,6 +72,8 @@ campocvc.addEventListener('keyup', function(){
     if(campocvc.value != "")
     {
     codigoSeguranca.innerHTML=campocvc.value;
+    erro[4].classList.add('hidden');
+    inputs[4].style.border='1px solid var(--Light-grayish-violet)';
     }
     else{
         codigoSeguranca.innerHTML="000";
@@ -74,58 +84,68 @@ campocvc.addEventListener('keyup', function(){
 
 
 botao.addEventListener('click', function(e) {
-    for(var i=0; i<inputs.length-1; i++){
-        if(inputs[i].value ===""){
-            erro[i].classList.remove('hidden');
+    
+    inputs.forEach((input,index) => {
+        if(input.value ===""){
+            
             e.preventDefault();
+            erro[index].classList.remove('hidden');
+            input.style.border='1px solid red';
         }
         else{
-            erro[i].classList.add('hidden');
+            erro[index].classList.add('hidden');
+            
+            
         }
 
-    }
-    
-    if((inputs[0].value !=="") && (inputs[1].value !=="") && (inputs[2].value !=="") && (inputs[3].value !=="") && (inputs[4].value !=="") && (botao.value != "Continue") ){
-        msnconfirmacao.classList.remove('hidden');
-        form.classList.add('hidden');
-        botao.value="Continue";
-        
-         
-    }
+       
+       
+            });
 
-    else
-    {
+
+      
+switch(estadobotao){
+    case false:
         msnconfirmacao.classList.add('hidden');
         form.classList.remove('hidden');
-        botao.value="Confirm";
+        botao.value="CONFIRM";
         limpaCampos();
-    }
-  
-});
-
-
-
-
-
+        estadobotao=true;
+        break;
+    case true:
+        if((inputs[0].value !=="") && (inputs[1].value !=="") && (inputs[2].value !=="") && (inputs[3].value !=="") && (inputs[4].value !=="") && (botao.value != "CONTINUE") ){
+            msnconfirmacao.classList.remove('hidden');
+            form.classList.add('hidden');
+            botao.value="CONTINUE";
+            
+            estadobotao=false;
+            break;
+           
+           
+            
+        }
+}
+    });
 
 
 function limpaCampos()
 {
     nomeCartao.innerHTML="Luiz F S Fagundes";
-    campoNome.innerHTML="";
+    campoNome.value="";
     
     numeroCartao.innerHTML="0000 0000 0000 0000";
-    campoNumero.innerHTML="";
+    campoNumero.value="";
     
     dataCartao.innerHTML="00/00";
-    campomes.innerHTML=""; 
-    campoano.innerHTML="";     
+    campomes.value=""; 
+    campoano.value="";     
     
     codigoSeguranca.innerHTML="000";
-    campocvc.innerHTML="";
-    inputs.forEach((input) => {
-        input.value="";
-    }); 
+    campocvc.value="";
+    
+    // inputs.forEach((input) => {
+    //     input.value="";
+    // }); 
 }
 
 
